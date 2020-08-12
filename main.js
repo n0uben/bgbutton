bd1154f95bcfa46cd3ab435294c9bc8f63a099fc
n0uben <benjamin.noureux@gmail.com>
2020-08-12 22:35:30 +0200

    new function to change main bg color on button click

--- main.js
+++ main.js
@@ -1,6 +1,20 @@
-const changeButtonText = (text, buttonId) => {
-    //if (text === String && buttonId === String) {
-        const buttonChangeBg = document.getElementById(buttonId);
-        buttonChangeBg.textContent = text;
-    //};
+let mainContainer = document.getElementById("main");
+
+const buttonChangeBackground = document.getElementById("change-bg");
+
+const colorTheme1 = ["264653", "2A9D8F", "E9C46A", "F4A261", "E76F51", "14213d", "e63946", "457b9d", "3d5a80"];
+
+const getRandomColor = (colorTheme) => {
+    let randomIndexColor = Math.floor(Math.random()*colorTheme.length);
+    return colorTheme[randomIndexColor];
 }
+
+const changeColor = () => {
+    event.preventDefault();
+    mainContainer.style.backgroundColor = "#" + getRandomColor(colorTheme1);
+}
+
+buttonChangeBackground.addEventListener('click', changeColor);
+
+
+
